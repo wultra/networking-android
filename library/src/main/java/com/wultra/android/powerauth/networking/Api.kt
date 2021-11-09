2446881f8e7740798380eed9dd5a76c70b49c2f0
Jan Kobersky <kober32@gmail.com>
2021-11-09 14:41:46 +0100

    Added cause of ApiHttpException when caused by another Exception (#10)

--- library/src/main/java/com/wultra/android/powerauth/networking/Api.kt
+++ library/src/main/java/com/wultra/android/powerauth/networking/Api.kt
@@ -185,7 +185,7 @@ abstract class Api(
                     }
                 } catch (e: Throwable) {
                     // do not allow the app to crash when unexpected body is returned
-                    listener.onFailure(ApiError(ApiHttpException(response)))
+                    listener.onFailure(ApiError(ApiHttpException(response, errorResponse = null, cause = e)))
                 }
             }
         })
