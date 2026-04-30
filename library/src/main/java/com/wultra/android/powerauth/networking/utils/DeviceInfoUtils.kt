3c2b539a83ed0c27995261b47e2fd6266c72e4fc
Marek Chrenko <marek.chrenko@wultra.com>
2026-04-30 14:04:26 +0200

    remove unused code

--- library/src/main/java/com/wultra/android/powerauth/networking/utils/DeviceInfoUtils.kt
+++ library/src/main/java/com/wultra/android/powerauth/networking/utils/DeviceInfoUtils.kt
@@ -18,7 +18,7 @@ package com.wultra.android.powerauth.networking.utils
 
 import android.content.Context
 import android.os.Build
-import java.util.*
+import java.util.Locale
 
 /**
  * Get currently selected locale.
@@ -29,60 +29,4 @@ fun Context.getCurrentLocale(): Locale {
     } else {
         this.resources.configuration.locale
     }
-}
-
-/**
- * Returns a well-formed IETF BCP 47 language tag representing
- * this locale.
- *
- * @author Tomas Kypta, tomas.kypta@wultra.com
- */
-fun Locale.toBcp47LanguageTag(): String {
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-        return this.toLanguageTag()
-    }
-
-    val sep = '-' // we will use a dash as per BCP 47
-    var language = this.language
-    var region = this.country
-    var variant = variant
-
-    // special case for Norwegian Nynorsk since "NY" cannot be a variant as per BCP 47
-    // this goes before the string matching since "NY" wont pass the variant checks
-    if (language == "no" && region == "NO" && variant == "NY") {
-        language = "nn"
-        region = "NO"
-        variant = ""
-    }
-
-    if (language.isEmpty() || !language.matches("\\p{Alpha}{2,8}".toRegex())) {
-        language = "und" // Follow the Locale#toLanguageTag() implementation
-        // which says to return "und" for Undetermined
-    } else if (language == "iw") {
-        language = "he" // correct deprecated "Hebrew"
-    } else if (language == "in") {
-        language = "id" // correct deprecated "Indonesian"
-    } else if (language == "ji") {
-        language = "yi" // correct deprecated "Yiddish"
-    }
-
-    // ensure valid country code, if not well formed, it's omitted
-    if (!region.matches("\\p{Alpha}{2}|\\p{Digit}{3}".toRegex())) {
-        region = ""
-    }
-
-    // variant subtags that begin with a letter must be at least 5 characters long
-    if (!variant.matches("\\p{Alnum}{5,8}|\\p{Digit}\\p{Alnum}{3}".toRegex())) {
-        variant = ""
-    }
-
-    val bcp47Tag = StringBuilder(language)
-    if (!region.isEmpty()) {
-        bcp47Tag.append(sep).append(region)
-    }
-    if (!variant.isEmpty()) {
-        bcp47Tag.append(sep).append(variant)
-    }
-
-    return bcp47Tag.toString()
 }
