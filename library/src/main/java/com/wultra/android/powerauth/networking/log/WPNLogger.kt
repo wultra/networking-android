36d3f72847a11a5102bf8790e29286a30dd38f05
Marek Chrenko <marek.chrenko@wultra.com>
2026-05-04 10:08:13 +0200

    remove duplicite import

--- library/src/main/java/com/wultra/android/powerauth/networking/log/WPNLogger.kt
+++ library/src/main/java/com/wultra/android/powerauth/networking/log/WPNLogger.kt
@@ -7,7 +7,6 @@ import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.Response
 import okio.Buffer
-import java.lang.StringBuilder
 import java.net.URL
 import java.util.concurrent.CopyOnWriteArrayList
 
