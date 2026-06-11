812b344b560f65e800540f479b3b59f10b6ecffa
Marek Chrenko <marek.chrenko@wultra.com>
2026-06-11 09:30:05 +0200

    Chore/develop to release 2.0.x (#90)

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -33,9 +33,9 @@ object Constants {
     }
 
     object Android {
-        const val compileSdkVersion = 35
-        const val targetSdkVersion = 35
-        const val minSdkVersion = 21
-        const val buildToolsVersion = "34.0.0"
+        const val compileSdkVersion = 36
+        const val targetSdkVersion = 36
+        const val minSdkVersion = 23
+        const val buildToolsVersion = "36.0.0"
     }
 }
